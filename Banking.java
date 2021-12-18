5039a58ef1240bf4515b60429f6ac43c32ba57e8
JC <84418447+JC79012@users.noreply.github.com>
2021-12-17 22:35:41 -0500

    Added comments

--- Banking.java
+++ Banking.java
@@ -47,31 +47,38 @@ class Access{
 		String pattern = "##,###.00";
 		DecimalFormat form = new DecimalFormat(pattern);
 		
-		
+		//collects user's name to use in Banking object 
 		System.out.print("Enter your name: ");
 		String name = scan.nextLine();
 		
+		//used in creating random balance value as 
+		//since there is no database to hold user balance
 		double min = 0; 
 		double max = 10000;	
 		double balance = min +(max-min)*rand.nextDouble();
 		
+		//create object with user's name and random balance amount to simulate what the feature would look like if it had actual user information stored 
 		Banking user = new Banking(name, balance);
 		
 		user.greetings();
 		
 		String answer="";
 		
+		//creates the loop that continously asks the user if they want to check their account
 		while(!"n".equals(answer)) {
+			
+			//asks and checks for accurate input to question 
 			do {
 				System.out.print("Would you like to view your account (y/n): ");
 				answer = scan.nextLine();
 			}while(!answer.equalsIgnoreCase("y")&&!answer.equalsIgnoreCase("n"));
 			
-			
+			//stops loop if user doesnt want to use project 
 			if(answer.equalsIgnoreCase("n")) {
 				break;
 			}
 			
+			//asks user to make a decision and checks for accurate input 
 			String choice;
 			do {
 				System.out.print("Would you like to Deposit(d), Withdraw(w) or view Balance(b)?: ");
@@ -79,10 +86,11 @@ class Access{
 				
 			}while(!choice.equalsIgnoreCase("d") && !choice.equalsIgnoreCase("w") && !choice.equalsIgnoreCase("b"));
 			
-			
+			//based on user selection, the Banking class method is called 
 			if(choice.equalsIgnoreCase("b")) {
 				System.out.println("Balance: $" + form.format(user.getBalance()));
 			}
+			//calls deposit method
 			else if(choice.equalsIgnoreCase("d")) {
 				System.out.print("Enter amount to deposit: $");
 				double deposit = scan.nextDouble();
@@ -90,6 +98,7 @@ class Access{
 				System.out.println("Deposit amount: $" + form.format(deposit));
 				scan.nextLine();
 			}
+			//calls withdraw 
 			else if(choice.equalsIgnoreCase("w")) {
 				double withdraw;
 				do {
@@ -102,6 +111,8 @@ class Access{
 			}
 			
 		}
+		
+		
 		System.out.println("Thank You, Session closed");
 		
 		scan.close();
